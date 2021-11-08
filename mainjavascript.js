3fac24be8fd7bafd21a051497b4b03090e326938
SamBeagles <92871003+SamBeagles@users.noreply.github.com>
2021-11-08 02:09:50 +0000

    Add files via upload

---
+++ mainjavascript.js
@@ -0,0 +1,5 @@
+/** Checkbox in HTML class="change", opens in a new tab for buttons1,2,3,4 */
+/** if you are on a page have buttons1,2,3,4 have constant colour change */
+/** Potato :D */
+var year = new Date();document.write(year.getFullYear());
+document.getElementsByClassName(".form").innerHTML= ".form1"
